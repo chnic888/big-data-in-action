5f94d57896ad14d24c92e5502b4519812b4b08bf
chnic888 <chnic888@gmail.com>
2024-04-06 01:42:34 +0800

    transfer the local rsa pub key to vm for ssh without password

--- README.md
+++ README.md
@@ -3,13 +3,12 @@
 ## Getting Started
 
 ### Prerequisites
-- Python 3.10
+- Ubuntu 22.04
+- Ansible 2.10
 
 ## Setup Cluster
 
 ### Install multipass
-
-#### Ubuntu(22.04)
 ```shell
 sudo snap install multipass
 
@@ -18,18 +17,13 @@ snap install lxd
 lxd init
 
 multipass set local.driver=lxd
-```
 
-#### MacOS
-```shell
-brew install --cask multipass
-
-multipass set local.driver=qemu
+ssh-keygen -t rsa -b 4096 -N "" -f id_rsa
 ```
 
 ### Create Cluster
 ```shell
 sudo nmcli connection add type bridge con-name localbr ifname localbr ipv4.method manual ipv4.addresses 10.13.31.1/24
-
+ 
 bash sbin/create_cluster.sh <instance number>
 ```
--- sbin/create_cluster.sh
+++ sbin/create_cluster.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <instance number> <>"
+    echo "Usage: $0 <instance number>"
     exit 1
 fi
 
+current_dir=$(dirname "$0")
 start_index=10
 count=$1
 
+cat ~/.ssh/id_rsa.pub >> "$current_dir/1.tmp"
+
 for ((i = 1; i <= count; i++));
 do
-    instance="vmw0$i"
+    instance="vm0$i"
     echo "Launch VM instance $instance."
     multipass launch jammy --name $instance -c 4 -m 4G -d 10G --network name=localbr,mode=manual
 
     mac_address=$(multipass exec $instance -- ip address | awk '/^[0-9]+:/ { current=$2; gsub(/:/,"",current); } /^[[:space:]]*link/ { mac=$2; } END { print mac }')
     sub_index=$((start_index + i))
 
-    echo "Create /etc/netplan/10-custom.yaml for $instance."
     multipass exec -n $instance -- sudo bash -c "cat << EOF > /etc/netplan/10-custom.yaml
 network:
     version: 2
@@ -29,8 +31,12 @@ network:
             addresses: [10.13.31.$sub_index/24]
 EOF"
 
-    echo "Apply netplan for $instance."
     multipass exec -n $instance -- sudo netplan apply >> /dev/null 2>&1
+    multipass transfer ./1.tmp $instance:/tmp/id_rsa.pub
+    multipass exec $instance -- bash -c 'mkdir -p ~/.ssh && cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys && rm /tmp/id_rsa.pub'
+
+    rm "$current_dir/1.tmp"
+    echo "10.13.31.$sub_index" >> "$current_dir/inventory"
 
     echo "Launch VM instance $instance successfully."
 done
