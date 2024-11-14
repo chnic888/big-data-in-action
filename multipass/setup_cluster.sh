a64cde28c520fc6fbdfe91ead8dbbccd5d949407
CH <chnic888@gmail.com>
2024-11-14 18:05:01 +0800

    update setup_cluster.sh

--- multipass/setup_cluster.sh
+++ multipass/setup_cluster.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+HADOOP_VERSION="hadoop-3.4.0"
+
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <instance number> <is replace source>"
     exit 1
 fi
 
-current_dir=$(dirname "$0")
 node_count=$1
 replace_source=$2
+
+function clean_up() {
+    rm -f "./ansible/inventory"
+    rm -f "./ansible/sources.list"
+}
 
 function pre_check {
     echo "Check OS and Arch..."
@@ -19,7 +25,7 @@ function pre_check {
         OS="macOS"
         ARCH=$(uname -m)
     else
-        echo "unknown OS and architecture..."
+        echo "Unknown OS and architecture..."
         exit 1
     fi
 
@@ -28,37 +34,41 @@ function pre_check {
 
 function generate_source_list() {
     if [[ "$replace_source" == "false" ]]; then
+        echo "Skip generate sources.list and will use default config..."
         return 0
     fi
 
     if [[ "$ARCH" == "aarch64" ]] || [[ "$ARCH" == "arm64" ]]; then
-        cp "${current_dir}/ansible/sources.list.aarch64" "${current_dir}/ansible/sources.list"
+        cp "./ansible/sources.list.aarch64" "./ansible/sources.list"
     else
-        cp "${current_dir}/ansible/sources.list.amd64" "${current_dir}/ansible/sources.list"
+        cp "./ansible/sources.list.amd64" "./ansible/sources.list"
     fi
 }
 
 function download_hadoop() {
-    echo "download hadoop..."
-    local url='https://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-3.4.0/'
-    file_name='hadoop-3.4.0.tar.gz'
+    local url="https://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/${HADOOP_VERSION}/"
+    file_name="${HADOOP_VERSION}.tar.gz"
 
     if [[ "$ARCH" == "aarch64" ]] || [[ "$ARCH" == "arm64" ]]; then
-      file_name='hadoop-3.4.0-aarch64.tar.gz'
+      file_name="${HADOOP_VERSION}-aarch64.tar.gz"
     fi
 
-    if [ ! -e "${current_dir}/hadoop/${file_name}" ]; then
-      wget -P "${current_dir}/hadoop" "${url}${file_name}"
+    if [ ! -e "./hadoop/${file_name}" ]; then
+      echo "${file_name} not found, start to download file..."
+      wget -P "./hadoop" "${url}${file_name}"
     else
-      echo "${file_name} exist..."
+      echo "File ${file_name} exist..."
+    fi
+
+    if [ ! -e "./hadoop/${HADOOP_VERSION}" ]; then
+      echo "extract ${file_name}..."
+      tar xf "./hadoop/${file_name}" -C ./hadoop
     fi
 }
 
 function launch_cluster() {
-    echo "launch multiplass cluster..."
-
-    echo "copy id_rsa.pub..."
-    cat ~/.ssh/id_rsa.pub > "$current_dir/1.tmp"
+    echo "Start to launch cluster..."
+    cat ~/.ssh/id_rsa.pub > "./1.tmp"
 
     for ((i = 1; i <= node_count; i++));
     do
@@ -72,28 +82,30 @@ function launch_cluster() {
           continue
         fi
 
-        echo "launch VM instance $instance..."
-        multipass launch jammy --name $instance -c 1 -m 2G -d 8G --mount "${current_dir}/hadoop/":/home/ubuntu
+        echo "Launch VM instance ${instance}..."
+        multipass launch jammy --name $instance -c 1 -m 2G -d 8G
+        multipass mount "./hadoop/${HADOOP_VERSION}" $instance:/home/ubuntu/hadoop
 
-        echo "copy id_rsa.pub to $instance..."
+        echo "Copy id_rsa.pub to ${instance}..."
         multipass transfer ./1.tmp $instance:/tmp/id_rsa.pub
         multipass exec $instance -- bash -c 'mkdir -p ~/.ssh && cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys && rm /tmp/id_rsa.pub'
 
         ip=$(multipass exec $instance -- bash -c "hostname -I | cut -d ' ' -f 1")
         echo "Write IP ${ip} to inventory file..."
-        echo "[vm]" > "$current_dir/ansible/inventory"
-        echo "${ip}" >> "$current_dir/ansible/inventory"
+        echo "[vm]" > "./ansible/inventory"
+        echo "${ip}" >> "./ansible/inventory"
 
-        echo "launch VM instance $instance successfully..."
+        echo "Launch VM instance ${instance} successfully..."
     done
 
-    rm "$current_dir/1.tmp"
+    rm "./1.tmp"
 }
 
 function install_dependency() {
     ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./ansible/inventory ./ansible/vm_update_upgrade.yaml --extra-vars "{'replace_source': $replace_source}"
 }
 
+clean_up
 pre_check
 generate_source_list
 download_hadoop
