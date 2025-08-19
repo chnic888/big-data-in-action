286f89b36930e37a8266448968d203e3a8b58b2e
CH <chnic888@gmail.com>
2025-08-19 22:16:24 +0800

    add flink hadoop shaded jar files

--- multipass/flink/install_flink.sh
+++ multipass/flink/install_flink.sh
@@ -2,6 +2,9 @@
 
 FLINK_VERSION="1.20.2"
 
+flink_shaded_hadoop_file_name="flink-shaded-hadoop-2-uber-2.8.3-10.0.jar"
+commons_cli_file_name="commons-cli-1.5.0.jar"
+
 function download_flink() {
     local flink_base_url="https://dlcdn.apache.org/flink/flink-$FLINK_VERSION"
     flink_file_name="flink-$FLINK_VERSION-bin-scala_2.12.tgz"
@@ -19,11 +22,37 @@ function download_flink() {
     fi
 }
 
+function download_flink_shaded_hadoop() {
+    if [ ! -e "./flink/archive/${flink_shaded_hadoop_file_name}" ]; then
+        echo "${flink_shaded_hadoop_file_name} not found, start to download file..."
+        wget -P "./flink/archive" "https://repo.maven.apache.org/maven2/org/apache/flink/flink-shaded-hadoop-2-uber/2.8.3-10.0/${flink_shaded_hadoop_file_name}"
+    else
+        echo "File ${flink_shaded_hadoop_file_name} exist..."
+    fi
+
+    if [ ! -e "./flink/archive/${commons_cli_file_name}" ]; then
+        echo "${commons_cli_file_name} not found, start to download file..."
+        wget -P "./flink/archive" "https://repo1.maven.org/maven2/commons-cli/commons-cli/1.5.0/${commons_cli_file_name}"
+    else
+        echo "File ${commons_cli_file_name} exist..."
+    fi
+}
+
 function mount_flink() {
     for host in $(ansible-inventory -i inventory --list | jq -r '.hadoop.hosts[]'); do
         echo "Mount ./flink/archive/flink-${FLINK_VERSION} on $host:/home/ubuntu/flink"
         multipass mount "./flink/archive/flink-${FLINK_VERSION}" "$host":/home/ubuntu/flink
     done
+}
+
+function transfer_hadoop_jars() {
+    for host in $(ansible-inventory -i inventory --list | jq -r '.hadoop.hosts[]'); do
+        echo "Transfer ./flink/archive/${flink_shaded_hadoop_file_name} on $host:/home/ubuntu/"
+        multipass transfer "./flink/archive/${flink_shaded_hadoop_file_name}" "$host":/home/ubuntu/
+
+        echo "Transfer ./flink/archive/${commons_cli_file_name} on $host:/home/ubuntu/"
+        multipass transfer "./flink/archive/${commons_cli_file_name}" "$host":/home/ubuntu/
+    done    
 }
 
 function install_flink() {
@@ -38,6 +67,8 @@ function unmount_flink() {
 }
 
 download_flink
+download_flink_shaded_hadoop
 mount_flink
+transfer_hadoop_jars
 install_flink
 unmount_flink
