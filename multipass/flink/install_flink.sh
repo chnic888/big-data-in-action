3b4c63e7886bf12b1d8b812db6c47834241add57
CH <chnic888@gmail.com>
2025-10-24 16:47:21 +0800

    add flink kafka connector jars

--- multipass/flink/install_flink.sh
+++ multipass/flink/install_flink.sh
@@ -4,6 +4,8 @@ FLINK_VERSION="1.20.2"
 
 flink_shaded_hadoop_file_name="flink-shaded-hadoop-2-uber-2.8.3-10.0.jar"
 commons_cli_file_name="commons-cli-1.5.0.jar"
+flink_connector_kafka_jar_name="flink-connector-kafka-1.17.2.jar"
+kafka_clients_jar_name="kafka-clients-3.9.1.jar"
 
 function download_flink() {
     local flink_base_url="https://dlcdn.apache.org/flink/flink-$FLINK_VERSION"
@@ -38,6 +40,22 @@ function download_flink_shaded_hadoop() {
     fi
 }
 
+function download_flink_connector() {
+    if [ ! -e "./flink/archive/${flink_connector_kafka_jar_name}" ]; then
+        echo "${flink_connector_kafka_jar_name} not found, start to download file..."
+        wget -P "./flink/archive" "https://repo1.maven.org/maven2/org/apache/flink/flink-connector-kafka/1.17.2/${flink_connector_kafka_jar_name}"
+    else
+        echo "File ${flink_connector_kafka_jar_name} exist..."
+    fi
+
+    if [ ! -e "./flink/archive/${kafka_clients_jar_name}" ]; then
+        echo "${kafka_clients_jar_name} not found, start to download file..."
+        wget -P "./flink/archive" "https://repo1.maven.org/maven2/org/apache/kafka/kafka-clients/3.9.1/${kafka_clients_jar_name}"
+    else
+        echo "File ${kafka_clients_jar_name} exist..."
+    fi
+}
+
 function mount_flink() {
     for host in $(ansible-inventory -i inventory --list | jq -r '.hadoop.hosts[]'); do
         echo "Mount ./flink/archive/flink-${FLINK_VERSION} on $host:/home/ubuntu/flink"
@@ -45,13 +63,19 @@ function mount_flink() {
     done
 }
 
-function transfer_hadoop_jars() {
+function transfer_dependent_jars() {
     for host in $(ansible-inventory -i inventory --list | jq -r '.hadoop.hosts[]'); do
         echo "Transfer ./flink/archive/${flink_shaded_hadoop_file_name} on $host:/home/ubuntu/"
         multipass transfer "./flink/archive/${flink_shaded_hadoop_file_name}" "$host":/home/ubuntu/
 
         echo "Transfer ./flink/archive/${commons_cli_file_name} on $host:/home/ubuntu/"
         multipass transfer "./flink/archive/${commons_cli_file_name}" "$host":/home/ubuntu/
+
+        echo "Transfer ./flink/archive/${flink_connector_kafka_jar_name} on $host:/home/ubuntu/"
+        multipass transfer "./flink/archive/${flink_connector_kafka_jar_name}" "$host":/home/ubuntu/
+
+        echo "Transfer ./flink/archive/${kafka_clients_jar_name} on $host:/home/ubuntu/"
+        multipass transfer "./flink/archive/${kafka_clients_jar_name}" "$host":/home/ubuntu/
     done    
 }
 
@@ -68,7 +92,8 @@ function unmount_flink() {
 
 download_flink
 download_flink_shaded_hadoop
+download_flink_connector
 mount_flink
-transfer_hadoop_jars
+transfer_dependent_jars
 install_flink
 unmount_flink
