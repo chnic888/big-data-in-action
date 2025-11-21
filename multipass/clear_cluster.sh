5cf4635c39c93a22a74a05d65dd83ebd6a35f7a7
CH <chnic888@gmail.com>
2025-11-21 16:12:35 +0800

    add doris and starrocks

--- multipass/clear_cluster.sh
+++ multipass/clear_cluster.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-multipass stop --all
-multipass delete --all
-multipass purge
+INSTANCES_TO_PURGE=$(multipass list | awk '/^vm/ {print $1}')
+
+if [ -n "$INSTANCES_TO_PURGE" ]; then
+        INSTANCES_TO_PURGE=$(multipass list | awk '/^vm/ {print $1}' | tr '\n' ' ')
+    
+    if [ -n "$INSTANCES_TO_PURGE" ]; then
+        echo "Stopping and deleting the following instances: $INSTANCES_TO_PURGE"
+        multipass delete $INSTANCES_TO_PURGE
+    else
+        echo "No instances starting with 'vm' found."
+    fi
+    multipass delete $INSTANCES_TO_PURGE
+else
+    echo "No instances starting with 'vm' found."
+fi
+
 multipass list
